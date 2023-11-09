2bf41ce3e46cfe228a58d37acbc1ea0972bca39e
Kihong <kks4517@naver.com>
2023-11-09 17:47:21 +0900

    Toby : Dispatcher Servlet으로 변경
    FrontController -> Dispatcher Servlet으로 변경
    HelloController -> @RequestMapping 추가
    hello() -> @GetMapping, @ResponseBody 추가

--- side/src/main/java/com/blanc/side/SideApplication.java
+++ side/src/main/java/com/blanc/side/SideApplication.java
@@ -2,18 +2,11 @@ package com.blanc.side;
 
 import com.blanc.side.toby.controller.HelloController;
 import com.blanc.side.toby.service.SimpleHelloService;
-import jakarta.servlet.ServletException;
-import jakarta.servlet.http.HttpServlet;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
-import java.io.IOException;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.server.WebServer;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
 import org.springframework.web.context.support.GenericWebApplicationContext;
+import org.springframework.web.servlet.DispatcherServlet;
 
 
 public class SideApplication {
@@ -26,26 +19,7 @@ public class SideApplication {
 
     ServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
     WebServer webServer = serverFactory.getWebServer(servletContext -> {
-
-      servletContext.addServlet("hello", new HttpServlet() {
-        @Override
-        protected void service(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-          if (request.getRequestURI().equals("/hello") && request.getMethod().equals(HttpMethod.GET.name())) {
-            String name = request.getParameter("name");
-
-            HelloController helloController = applicationContext.getBean(HelloController.class);
-
-            String helloMsg = helloController.hello(name);
-
-            response.setStatus(HttpStatus.OK.value());
-            response.setContentType(MediaType.TEXT_PLAIN_VALUE);
-            response.getWriter().println(helloMsg);
-          } else {
-            response.setStatus(HttpStatus.NOT_FOUND.value());
-          }
-        }
-      }).addMapping("/*");
+      servletContext.addServlet("DispatcherServlet", new DispatcherServlet(applicationContext)).addMapping("/*");
     });
     webServer.start();
   }
