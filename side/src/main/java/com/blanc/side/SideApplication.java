07b31184edf784319e3bec768be09e4a51fb8c2c
Kihong <kks4517@naver.com>
2023-11-09 17:29:20 +0900

    Toby : Servlet 추가
    response 상태코드, Content-Type, Body 설정

--- side/src/main/java/com/blanc/side/SideApplication.java
+++ side/src/main/java/com/blanc/side/SideApplication.java
@@ -1,8 +1,15 @@
 package com.blanc.side;
 
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.server.WebServer;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 
 
 public class SideApplication {
@@ -10,7 +17,15 @@ public class SideApplication {
   public static void main(String[] args) {
     ServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
     WebServer webServer = serverFactory.getWebServer(servletContext -> {
-			//Servlet 등록할 공간
+      servletContext.addServlet("hello", new HttpServlet() {
+        @Override
+        protected void service(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+          response.setStatus(HttpStatus.OK.value());
+          response.setContentType(MediaType.TEXT_PLAIN_VALUE);
+          response.getWriter().write("Hello SpringBoot");
+        }
+      }).addMapping("/hello");
     });
     webServer.start();
   }
