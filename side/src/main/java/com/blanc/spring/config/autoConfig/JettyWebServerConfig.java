207a266c3d0e74d9399881900136e766a3902b20
Kihong <kks4517@naver.com>
2023-11-13 11:34:27 +0900

    toby: Jetty 의존성추가, AutoConfig 설정

--- side/src/main/java/com/blanc/spring/config/autoConfig/JettyWebServerConfig.java
+++ side/src/main/java/com/blanc/spring/config/autoConfig/JettyWebServerConfig.java
@@ -1,15 +1,15 @@
 package com.blanc.spring.config.autoConfig;
 
 import com.blanc.spring.config.MyAutoConfiguration;
-import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.embedded.jetty.JettyServletWebServerFactory;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
 
 @MyAutoConfiguration
-public class TomcatWebServerConfig {
+public class JettyWebServerConfig {
 
-  @Bean("tomcatWebServerFactory")
+  @Bean("jettyWebServerFactory")
   public ServletWebServerFactory serverFactory() {
-    return new TomcatServletWebServerFactory();
+    return new JettyServletWebServerFactory();
   }
 }
