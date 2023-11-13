ad2fceced7a989cc148227142d136772daa5d318
Kihong <kks4517@naver.com>
2023-11-13 16:22:10 +0900

    toby: 유저구성정보를 통한 조건부 Conditional Bean 생성

--- side/src/main/java/com/blanc/spring/config/autoConfig/JettyWebServerConfig.java
+++ side/src/main/java/com/blanc/spring/config/autoConfig/JettyWebServerConfig.java
@@ -2,6 +2,7 @@ package com.blanc.spring.config.autoConfig;
 
 import com.blanc.spring.config.ConditionalMyOnClass;
 import com.blanc.spring.config.MyAutoConfiguration;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.web.embedded.jetty.JettyServletWebServerFactory;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
@@ -11,6 +12,7 @@ import org.springframework.context.annotation.Bean;
 public class JettyWebServerConfig {
 
   @Bean("jettyWebServerFactory")
+  @ConditionalOnMissingBean
   public ServletWebServerFactory serverFactory() {
     return new JettyServletWebServerFactory();
   }
